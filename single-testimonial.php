d8db7f5a42510048afc22ec4bca6e11f9caaaf7d
Matt Ryan <msr1222@gmail.com>
2024-02-14 16:59:02 -0500

    Add single testimonial template

---
+++ single-testimonial.php
@@ -0,0 +1,35 @@
+<?php
+
+get_header();
+global $post;  
+$post_id = ( empty( $post->ID ) ) ? get_the_ID() : $post->ID;
+
+// Add preferred internal Kadence block pattern page header to page template file
+block_template_part( 'html-title' );
+
+// Get the post meta data
+$post_title = get_the_title( $post_id );
+$testimonial_company = rwmb_meta( 'penncat_testimonial_company' );
+$testimonial_title = rwmb_meta( 'penncat_testimonial_title' );
+// Get the post thumbnail URL
+$post_thumbnail_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
+
+?>
+<div class="testimonial-container" style="margin: 2em 15em 2em; ">
+    <div class="testimonial-wrapper">
+        <?php the_content( $post_id ); ?>
+        <img src="<?php echo esc_url($post_thumbnail_url); ?>" alt="<?php echo esc_attr($post_title); ?>">
+        <div class="testimonial-meta">
+            <h2><?php echo esc_html($post_title); ?></h2>
+            <?php echo esc_html($testimonial_company) . ', ' . esc_html($testimonial_title); ?>
+        </div>
+    </div>
+</div>	
+
+<?php
+
+// Add preferred internal Kadence block pattern page footer / CTA to page template file
+block_template_part( 'html-request-free-quote-box' );
+get_footer();
+
+
