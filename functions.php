c3151ba624d0116e5ae4a0ab0b65acd81184e2ef
Matt Ryan <msr1222@gmail.com>
2024-02-14 17:00:53 -0500

    Remove scripts add logic

--- functions.php
+++ functions.php
@@ -26,51 +26,7 @@ function custom_query_vars($query_vars) {
 }
 add_filter('query_vars', __NAMESPACE__ . '\custom_query_vars');
 
-  
-
-function add_id_to_links_script() {
-    ?>
-    <script type="text/javascript">
-        document.addEventListener("DOMContentLoaded", function() {
-        function addIdToLinks() {
-            var links = document.querySelectorAll(\'a[href*="https://api.leadconnectorhq.com/widget/form/VHNpmgrC5JUoLkP1ptkx"]\');
-            links.forEach(function(link) {
-                link.setAttribute("id", "get-free-quote-link");
-            });
-            var buttons = document.querySelectorAll(\'button[href*="https://api.leadconnectorhq.com/widget/form/VHNpmgrC5JUoLkP1ptkx"]\');
-            buttons.forEach(function(button) {
-                button.setAttribute("id", "get-free-quote-link");
-            });
-        }
-        addIdToLinks();
-        document.addEventListener("ajaxComplete", addIdToLinks);
-        });
-    </script>
-    <?php
-}
-
-function add_fa_event_tracker() {
-    ?>
-    <script>
-        window.addEventListener('load', (event) => {
-        document.getElementById('get-free-quote-link').addEventListener('click', () => {
-            fathom.trackEvent('Get free quote link clicked');
-        });
-        });
-    </script>
-    <?php
-}
-    
-add_action('wp_footer', __NAMESPACE__ . '\add_id_to_links_script');
-add_action('wp_footer', __NAMESPACE__ . '\add_fa_event_tracker' );
-
-
 function penncat_add_woocommerce_support() {
     add_theme_support( 'woocommerce' );
 }
 add_action( 'after_setup_theme', __NAMESPACE__ . '\penncat_add_woocommerce_support' );
-
-// add_action( 'after_setup_theme', __NAMESPACE__ . '\penncat_add_block_template_part_support' );
-// function penncat_add_block_template_part_support() {
-//     add_theme_support( 'block-template-parts' );
-// }
